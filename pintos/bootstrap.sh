2ea5efe9bbfdda4f3076916715a8788378233069
Stefano Taillefert <stefano.taille@gmail.com>
2020-04-01 16:28:01 +0200

    Add extra build files to .gitignore, add logout alias

--- pintos/bootstrap.sh
+++ pintos/bootstrap.sh
@@ -9,6 +9,7 @@ export PATH
 export PS1='\${debian_chroot:+(\$debian_chroot)}\[\e[1;31m\]\u@\h:\[\e[0m\]\w\\$ '
 
 alias ls='ls -la'
+alias vagrant='echo "Closing the connection for you..."; logout'
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 EOF
