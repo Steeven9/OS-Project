3db73c6a385d3727ffda32b500b95ea6d53d4af8
Stefano Taillefert <stefano.taille@gmail.com>
2020-04-01 16:16:13 +0200

    Remove build files

--- pintos/pintos-env/pintos/threads/build/tests/threads/print_sorted.d
+++
@@ -1,5 +0,0 @@
-tests/threads/print_sorted.o: ../../tests/threads/print_sorted.c \
- ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
- ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
- ../../lib/kernel/stdio.h ../../tests/threads/../../lib/kernel/list.h \
- ../../tests/threads/listpop.h
